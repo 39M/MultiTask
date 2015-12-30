9bc44f8edb3a7ebcd8e0edb174cf99c402c37174
Jiaqi Wang <xwszc7@gmail.com>
2015-12-30 19:28:54 +0800

    ball move more slowly

--- Assets/Scene/Main/MiniGame/KeepItUp/KeepItUpBallMove.cs
+++ Assets/Scene/Main/MiniGame/KeepItUp/KeepItUpBallMove.cs
@@ -9,7 +9,7 @@ public class KeepItUpBallMove : MonoBehaviour
     void Start()
     {
         Vector2 direction = new Vector2(Random.Range(0.25f, 0.75f), Random.Range(0.3f, 0.7f));
-        gameObject.GetComponent<Rigidbody2D>().AddForce(direction.normalized / 2f, ForceMode2D.Impulse);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(direction.normalized / 3f, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
