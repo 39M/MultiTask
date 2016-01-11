6f87d551512379c4951f6cf2bebff475c719f4ba
Jiaqi Wang <xwszc7@gmail.com>
2016-01-11 14:20:33 +0800

    set difficulty

--- Assets/Scene/Main/MiniGame/Balance/Balance.cs
+++ Assets/Scene/Main/MiniGame/Balance/Balance.cs
@@ -5,6 +5,8 @@ public class Balance : BaseGame
 {
     public GameObject plantform;
     public GameObject ball;
+    float platformLength;
+    float angularDrag;
 
     public override void Start()
     {
@@ -14,12 +16,16 @@ public class Balance : BaseGame
         plantform.GetComponent<HingeJoint2D>().connectedAnchor += new Vector2(offset, 0);
         plantform.GetComponent<PlantformController>().isLeft = isLeft;
         plantform.GetComponent<PlantformController>().gameController = this;
-        plantform.transform.localScale = new Vector3(Mathf.Clamp(600 * Mathf.Pow(0.965f, difficulty), 200f, 600f), 40f, 1f);
 
         ball = CreateGameObject(ball);
         ball.transform.Translate(new Vector3(Random.Range(0.1f, 0.5f) * (Random.value > 0.5 ? 1 : -1), 0));
-        ball.GetComponent<Rigidbody2D>().angularDrag = Mathf.Clamp(10 * Mathf.Pow(0.925f, difficulty), 0.1f, 10f);
 
+    }
+
+    public void SetDifficulty()
+    {
+        platformLength = Mathf.Clamp(600 * Mathf.Pow(0.975f, difficulty), 200f, 600f);
+        angularDrag = Mathf.Clamp(10 * Mathf.Pow(0.925f, difficulty), 0.5f, 10f);
     }
 
     public override void Update()
@@ -35,9 +41,9 @@ public class Balance : BaseGame
             return;
         }
 
-        // Difficulty setting for debug
-        //plantform.transform.localScale = new Vector3(Mathf.Clamp(600 * Mathf.Pow(0.965f, difficulty), 200f, 600f), 40f, 1f);
-        //ball.GetComponent<Rigidbody2D>().angularDrag = Mathf.Clamp(10 * Mathf.Pow(0.925f, difficulty), 0.1f, 10f);
+        SetDifficulty();
+        plantform.transform.localScale = new Vector3(platformLength, 40f, 1f);
+        ball.GetComponent<Rigidbody2D>().angularDrag = angularDrag;
 
         if ((ball.transform.position - plantform.transform.position).magnitude > plantform.transform.localScale.x / 200f + 0.3f)
         {
