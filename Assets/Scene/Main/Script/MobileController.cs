2b61fd5f9c67359afc8853dbb322750c72d9163a
Jiaqi Wang <xwszc7@gmail.com>
2016-01-14 20:59:10 +0800

    mobile controller init

---
+++ Assets/Scene/Main/Script/MobileController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        foreach (var touch in Input.touches)
+        {
+            /* ToDo */
+        }
+    }
+}
