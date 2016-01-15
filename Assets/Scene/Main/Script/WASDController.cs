46f153d9d41486b12c033c637c3eb1a4f4e838dd
Jiaqi Wang <xwszc7@gmail.com>
2016-01-15 16:16:31 +0800

    yet another approach

--- Assets/Scene/Main/Script/WASDController.cs
+++ Assets/Scene/Main/Script/WASDController.cs
@@ -8,8 +8,9 @@ public class WASDController : BaseController
     bool touching = false;
     Touch currentTouch;
     int currentTouchID;
-    Vector2 touchBeginPosition;
-    Vector2 moveDirection;
+    //Vector2 touchBeginPosition;
+    //Vector2 moveDirection;
+    Vector3 moveTo;
 
     public override void Start()
     {
@@ -19,38 +20,41 @@ public class WASDController : BaseController
 
     public virtual void Update()
     {
-        if (!touching)
-        {
-            if (TouchAnyWhere(TouchPhase.Began))
-            {
-                foreach (var touch in Input.touches)
-                {
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        currentTouchID = touch.fingerId;
-                        touchBeginPosition = touch.position;
-                        break;
-                    }
-                }
-                touching = true;
-            }
-        }
+        //if (!touching)
+        //{
+        //    if (TouchAnyWhere())
+        //    {
+        //        foreach (var touch in Input.touches)
+        //        {
+        //            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+        //            {
+        //                currentTouchID = touch.fingerId;
+        //                //touchBeginPosition = touch.position;
+        //                touching = true;
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
 
-        if (touching)
-        {
-            currentTouch = Input.GetTouch(currentTouchID);
-            if (currentTouch.phase == TouchPhase.Ended || currentTouch.position.x <= baseGame.screenStartX ||
-                currentTouch.position.x >= baseGame.screenEndX)
-            {
-                touching = false;
-            }
-            else
-            {
-                moveDirection = currentTouch.position - touchBeginPosition;
-                moveDirection.x = Mathf.Clamp(moveDirection.x, -195, 195) / 13;
-                moveDirection.y = Mathf.Clamp(moveDirection.y, -195, 195) / 13;
-            }
-        }
+        //if (touching)
+        //{
+        //    currentTouch = Input.GetTouch(currentTouchID);
+        //    if (currentTouch.phase == TouchPhase.Ended || currentTouch.phase == TouchPhase.Canceled ||
+        //        currentTouch.position.x <= baseGame.screenStartX ||
+        //        currentTouch.position.x >= baseGame.screenEndX)
+        //    {
+        //        touching = false;
+        //    }
+        //    else
+        //    {
+        //        //moveDirection = currentTouch.position - touchBeginPosition;
+        //        //moveDirection.x = Mathf.Clamp(moveDirection.x, -195, 195) / 13;
+        //        //moveDirection.y = Mathf.Clamp(moveDirection.y, -195, 195) / 13;
+
+        //        moveTo = (Camera.main.ScreenToWorldPoint(currentTouch.position) - transform.position);
+        //    }
+        //}
     }
 
     public virtual void FixedUpdate()
@@ -65,10 +69,23 @@ public class WASDController : BaseController
         //speed = 5 * Time.deltaTime;
         speed = 15f;
 
-        if (touching)
+        //if (touching)
+        //{
+        //    rb.AddForce(moveDirection);
+        //}
+
+        if (TouchAnyWhere())
         {
-            //rb.AddForce(moveDirection);
-            var moveTo = (Camera.main.ScreenToWorldPoint(currentTouch.position) - transform.position);
+            foreach (var touch in Input.touches)
+            {
+                if ((touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled &&
+                touch.position.x > baseGame.screenStartX && touch.position.x < baseGame.screenEndX))
+                {
+                    moveTo = (Camera.main.ScreenToWorldPoint(touch.position) - transform.position);
+                    break;
+                }
+            }
+
             moveTo.x = Mathf.Clamp(moveTo.x, -2, 2) * 7.5f;
             moveTo.y = Mathf.Clamp(moveTo.y, -2, 2) * 7.5f;
             rb.AddForce(moveTo);
