d191a2828b426bfcd4f22535d5624574cd58cf09
Jiaqi Wang <xwszc7@gmail.com>
2016-01-31 23:35:25 +0800

    clean

---
+++ Assets/Scene/Main/MiniGame/EatPokcy/EatPocky.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EatPocky : BaseGame
+{
+
+    public override void Start()
+    {
+        base.Start();
+
+    }
+
+    public void SetDifficulty()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void End()
+    {
+        destroy = true;
+
+        Destroy(gameObject);
+    }
+}
